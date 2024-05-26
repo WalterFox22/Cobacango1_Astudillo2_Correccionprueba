7dcaab9f9ec0c26977359c315cd1bdfc54dda598
Anthony <anthony.astudillo@epn.edu.ec>
2024-05-26 15:36:18 -0500

    CAMBIO UNO

---
+++ src/Hoteles.java
@@ -0,0 +1,58 @@
+public class Hoteles {
+    String nombre;
+    String direccion;
+    int estrellas;
+    int num_habitaciones;
+    Double precio_noche;
+
+    public Hoteles() {
+    }
+
+    public Hoteles(String nombre, String direccion, int estrellas, int num_habitaciones, Double precio_noche) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.estrellas = estrellas;
+        this.num_habitaciones = num_habitaciones;
+        this.precio_noche = precio_noche;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getEstrellas() {
+        return estrellas;
+    }
+
+    public void setEstrellas(int estrellas) {
+        this.estrellas = estrellas;
+    }
+
+    public int getNum_habitaciones() {
+        return num_habitaciones;
+    }
+
+    public void setNum_habitaciones(int num_habitaciones) {
+        this.num_habitaciones = num_habitaciones;
+    }
+
+    public Double getPrecio_noche() {
+        return precio_noche;
+    }
+
+    public void setPrecio_noche(Double precio_noche) {
+        this.precio_noche = precio_noche;
+    }
+}
