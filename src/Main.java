6a3aa8d1d4aaeeb1a07d5222cdad4ff6da0c181d
Anthony <anthony.astudillo@epn.edu.ec>
2024-05-26 16:00:44 -0500

    CAMBIO DOS

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("-------- Correccion de la prueba --------");
 
+        Hoteles hotel1 = new Hoteles("Marriot", "Francisco de Orellana", 5, 259, 450.00);
+        Hoteles hotel2 = new Hoteles(null, null, 0, 0, 0.0);
+        Hoteles hotel3 = new Hoteles(null, null, 0, 0, 0.0);
+        Hoteles hotel4 = new Hoteles("Hilton", "Ejido", 4, 120, 320.0);
+        Hoteles hotel5 = new Hoteles("Hotel Quito", "Gonzales Suarez", 4, 328, 129.0);
     }
 }
